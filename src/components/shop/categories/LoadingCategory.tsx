73f8ff52da4f16da5d0ab7fb4880f9276d006706
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-02-29 23:50:24 +0800

    Add Loading component for loading status of categories

---
+++ src/components/shop/categories/LoadingCategory.tsx
@@ -0,0 +1,11 @@
+import LoadingSvg from "/Loading.svg";
+
+const LoadingCategory = () => {
+  return (
+    <div className="flex justify-center">
+      <img src={LoadingSvg} />
+    </div>
+  );
+};
+
+export default LoadingCategory;
